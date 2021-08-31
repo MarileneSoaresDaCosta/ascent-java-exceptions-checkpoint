59775c58d408e1853a76edd0051f8aa1e63d15ef
PaulBuske <paul.a.buske.mil@swf.army.mil>
2021-08-31 15:56:10 -0500

    Submission for Paul B. and Jonathon P.

--- src/main/java/com/galvanize/ZipCodeProcessor.java
+++ src/main/java/com/galvanize/ZipCodeProcessor.java
@@ -10,5 +10,18 @@ public class ZipCodeProcessor {
     }
 
     // write your code below here...
+    public String process(String zipcode) {
+        try {
+            this.verifier.verify(zipcode);
+        } catch (Exception e) {
+            return (e.getMessage().equals("ERRCODE 21: INPUT_TOO_LONG") || e.getMessage().equals("ERRCODE 22: INPUT_TOO_SHORT")) ?
+                    "The zip code you entered was the wrong length." :
+                    (e.getMessage().equals("ERRCODE 27: NO_SERVICE")) ?
+                            "We're sorry, but the zip code you entered is out of our range." :
+                            "We're sorry, but there was an unknown exception.";
+        }
+        return "Thank you!  Your package will arrive soon.";
+
+    }
 
 }
--- src/test/java/com/galvanize/ZipCodeProcessorTest.java
+++ src/test/java/com/galvanize/ZipCodeProcessorTest.java
@@ -1,15 +1,42 @@
 package com.galvanize;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ZipCodeProcessorTest {
+    ZipCodeProcessor zipCodeProcessor;
+
+    @BeforeEach
+    void init() {
+        zipCodeProcessor = new ZipCodeProcessor(new Verifier());
+    }
 
     // write your tests here
     @Test
-    public void assertTrue() {
-        assertEquals(true, true);
+    public void processReturnsCorrectStringWhenZipCodeLengthIsCorrect() {
+        assertEquals("Thank you!  Your package will arrive soon.", zipCodeProcessor.process("80302"));
+    }
+
+    @Test
+    public void processReturnsWrongLengthStringWhenZipCodeIsNotCorrectLength() {
+        assertEquals(
+                "The zip code you entered was the wrong length.",
+                zipCodeProcessor.process("2345678")
+        );
+        assertEquals(
+                "The zip code you entered was the wrong length.",
+                zipCodeProcessor.process("321")
+        );
+    }
+
+    @Test
+    public void processReturnsOutOfRangeStringWhenZipCodeIsOutOfRange() {
+        assertEquals(
+                "We're sorry, but the zip code you entered is out of our range.",
+                zipCodeProcessor.process("12234")
+        );
     }
 
 }
